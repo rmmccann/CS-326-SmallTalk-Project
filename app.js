8b39e93062bec8eba1ec8a210f96df3cf42a91ed
lDantonl <derek5276@gmail.com>
2013-02-26 17:42:31 -0500

    reformated the app.js file

--- app.js
+++ app.js
@@ -4,30 +4,36 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var routes = require('./routes')
+var user = require('./routes/user')
+var http = require('http')
+var path = require('path');
 
 var app = express();
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
-});
+app.configure(
+	function()
+	{
+		app.set('port', process.env.PORT || 3000);
+		app.set('views', __dirname + '/views');
+		app.set('view engine', 'ejs');
+		app.use(express.favicon());
+		app.use(express.logger('dev'));
+		app.use(express.bodyParser());
+		app.use(express.methodOverride());
+		app.use(express.cookieParser('your secret here'));
+		app.use(express.session());
+		app.use(app.router);
+		app.use(express.static(path.join(__dirname, 'public')));
+	}
+);
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
+app.configure('development', 
+	function()
+	{
+		app.use(express.errorHandler());
+	}
+);
 
 app.get('/', routes.index);
 app.get('/signup', index.signup);
@@ -36,6 +42,9 @@ app.get('/:user/followers', user.followers);
 app.get('/:user/following', user.following);
 app.get('/feed/:hashtag', hashfeed.index);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
+http.createServer(app).listen(app.get('port'), 
+	function()
+	{
+		console.log("Express server listening on port " + app.get('port'));
+	}
+);
