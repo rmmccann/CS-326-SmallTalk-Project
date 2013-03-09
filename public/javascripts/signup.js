c91a61da94ddc1355b0efe9861a7542bba153748
Ryan McCann <mccann.ryan.m@gmail.com>
2013-03-09 03:27:20 -0500

    Finished the majority of signup.ejs including external css and js.

---
+++ public/javascripts/signup.js
@@ -0,0 +1,31 @@
+$(document).ready(function()
+{
+	$("input:text").focus(function(){
+		if(this.value===this.placeholder) this.value="";
+	});
+	$("input:text").blur(function(){
+		if(this.value==="") this.value=this.placeholder;
+	});
+
+	$("#submitButton").click(function(){
+		//TODO validate form:
+		//		-call checkpass()
+		//		-run username through a regex
+	});
+
+	//check if passwords match, in realtime, and display/hide the checkmark. Also check on blur (if user copy-pastes it, for instance).
+	$("#validatePass").keyup(checkpass).blur(checkpass); //yay chaining!
+	function checkpass()
+	{
+		if($(this).val() === $("#password").val())
+		{
+			$("#ok").show();
+			return true;
+		}
+		else
+		{
+			$("#ok").hide();
+			return false;
+		}
+	}
+});
