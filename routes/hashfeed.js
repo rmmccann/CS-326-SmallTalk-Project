e57c0fffa97776cb3ca30b0884e38b50c95e696d
JustinCaseyIvey <Justin.Casey.Ivey@gmail.com>
2013-03-10 17:01:23 -0400

    removed some libraries we didn't need

--- routes/hashfeed.js
+++ routes/hashfeed.js
@@ -1,4 +1,4 @@
-var Tweet = require("../lib/Post");
+var Post = require("../lib/Post");
 /*
  * GET hash feed.
  */
@@ -8,7 +8,7 @@ exports.feed = function(req, res)
 	var language = req.param("language");
 	language.toLowerCase();
 
-	
+
 	if(hashtag == undefined){
 		hashtag = "All"
 	}
