a75d2f3c72dc0a394b521a328a03b871b5775c48
Ryan McCann <mccann.ryan.m@gmail.com>
2013-04-05 13:53:22 -0400

    Added Schema and Database files.

---
+++ schema.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS Posts;
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Follows;
+
+CREATE TABLE Posts(id integer PRIMARY KEY NOT NULL, author_id integer, content text);
+CREATE TABLE Users(id integer PRIMARY KEY NOT NULL, username varchar(30), firstname varchar(30), lastname varchar(30), email varchar(30), password varchar(30));
+CREATE TABLE Follows(follower_id integer, followed_id integer);
+
+-- Test Data
+INSERT INTO Users values(NULL, 'rmmccann', 'Ryan', 'McCann', 'rmmccann@student.umass.edu', 'password');
+INSERT INTO Users values(NULL, 'danton', 'Derek', 'Anton', 'danton@student.umass.edu', 'password');
+INSERT INTO Users values(NULL, 'jcivey', 'Justin', 'Ivey', 'jcivey@student.umass.edu', 'password');
+INSERT INTO Users values(NULL, 'srojee', 'Scott', 'Rojee', 'srojee@student.umass.edu', 'password');
+
+-- Lets add some follower/following relations
+INSERT INTO Follows values(1,2);
+INSERT INTO Follows values(1,3);
+INSERT INTO Follows values(2,1);
+INSERT INTO Follows values(2,3);
+INSERT INTO Follows values(3,1);
+INSERT INTO Follows values(3,4);
+
+-- To find someone's followers
+--SELECT B.* FROM Users A, Users B, Follows F WHERE F.follower_id=A.id AND F.followed_id=B.id AND A.id=1;
